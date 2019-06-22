96cfb6a8781848210557a8bceffe703156ca0608
Yohei Koga <monotaro_EX@hotmail.com>
2019-06-23 01:03:59 +0900

    debug code

--- demo.py
+++ demo.py
@@ -138,6 +138,10 @@ while True:
     boxes = allboxes[:,:4]
     scores = allboxes[:,4]
     cls_inds = allboxes[:,5]
+    #debug
+    boxes = boxes[0]
+    scores = scores[0]
+    cls_inds = cls_inds[0]
     print('\n'.join(['pos:{}, ids:{}, score:{:.3f}'.format('(%.1f,%.1f,%.1f,%.1f)' % (o[0],o[1],o[2],o[3]) \
             ,labels[int(oo)],ooo) for o,oo,ooo in zip(boxes,cls_inds,scores)]))
     fps = 1.0 / float(loop_time) if cam >= 0 else -1
